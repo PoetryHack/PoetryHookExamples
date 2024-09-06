272e1d5d2acc0de6b2e15a956a352ae82ae3f1f2
jada <baller@thefunction>
2024-09-06 14:10:58 -0400

    default agent that does nothing yet

---
+++ src/main/java/net/poetryhack/poetryhook/examples/exampleagent/AgentMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created: 09.06.2024
+ */
+
+package net.poetryhack.poetryhook.examples.exampleagent;
+
+import java.lang.instrument.Instrumentation;
+
+public class AgentMain {
+    public static void agentmain(String ignoredAgentArgs, Instrumentation inst) {}
+}

f6bea857fe8bc3e4ead34f19a9e1842ec0b5b639
jada <baller@thefunction>
2024-09-06 12:42:08 -0400

    initial commit

---
+++ src/main/java/net/poetryhack/poetryhook/example/AppMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created: 09.06.2024
+ */
+
+package net.poetryhack.poetryhook.example;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}

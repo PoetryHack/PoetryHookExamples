63f7f6c4c813ae5b767fa56e298445f38d86b228
jada <baller@thefunction>
2024-09-12 21:24:02 -0400

    add the annotation to fix the fact the method wasn't being found change parts of hook to make the fact it is changed more obvious

--- src/main/java/net/poetryhack/poetryhook/examples/exampleagent/HookMeMixin.java
+++ src/main/java/net/poetryhack/poetryhook/examples/exampleagent/HookMeMixin.java
@@ -6,6 +6,7 @@ package net.poetryhack.poetryhook.examples.exampleagent;
 
 import net.poetryhack.poetryhook.MixinBase;
 import net.poetryhack.poetryhook.annotations.Inject;
+import net.poetryhack.poetryhook.annotations.ObjectWrapper;
 import net.poetryhack.poetryhook.annotations.StringMixin;
 
 import java.lang.reflect.Field;
@@ -18,9 +19,11 @@ public class HookMeMixin implements MixinBase {
     @Inject(
             value = "printStringToChange"
     )
-    public static void printStringToChangeHook(Object thisthis) throws NoSuchFieldException, IllegalAccessException {
-        Class<?> clazz = thisthis.getClass();  // todo wrapper for other objects which you cannot add as dependency
-        Field f = clazz.getField("stringToChange");
+    public static void printStringToChangeHook(
+            @ObjectWrapper("net.poetryhack.poetryhook.examples.exampleapp.HookMe") Object thisthis
+    ) throws NoSuchFieldException, IllegalAccessException {
+        Class<?> clazz = thisthis.getClass();
+        Field f = clazz.getDeclaredField("stringToChange");
         f.setAccessible(true);
         if (Objects.equals(f.get(thisthis), "unchanged hookMeObj1 string")) {
             f.set(thisthis, "hookMeObj1 string changed by hook while injected");
@@ -30,11 +33,13 @@ public class HookMeMixin implements MixinBase {
     @Inject(
             value = "getCounterMethod"
     )
-    public static void getCounterMethodHook(Object thisthis) throws NoSuchFieldException, IllegalAccessException {
+    public static void getCounterMethodHook(
+            @ObjectWrapper("net.poetryhack.poetryhook.examples.exampleapp.HookMe") Object thisthis
+    ) throws NoSuchFieldException, IllegalAccessException {
         Class<?> clazz = thisthis.getClass();
-        Field f = clazz.getField("counter");
+        Field f = clazz.getDeclaredField("counter");
         f.setAccessible(true);
         int val = (int) f.get(thisthis);
-        f.set(thisthis, val * 3);
+        f.set(thisthis, (val + 12) * 3);
     }
 }
